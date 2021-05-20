4f3fb9e2a33881e371b475c3805daeb81d4a195d
Rafael <rafa.shiga@gmail.com>
2021-05-20 10:50:00 -0300

    test(checkbox): ensure toggleCheckbox emit an event

--- src/app/shared/checkbox/checkbox.component.spec.ts
+++ src/app/shared/checkbox/checkbox.component.spec.ts
@@ -21,4 +21,13 @@ describe('CheckboxComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should toggleCheckbox emit an event', () => {
+    const checkboxClickSpy = spyOn(
+      component.checkboxClick,
+      'emit'
+    ).and.callThrough();
+    component.toggleCheckbox('any_event');
+    expect(checkboxClickSpy).toHaveBeenCalled();
+  });
 });
